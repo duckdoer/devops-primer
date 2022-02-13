b6a7549557ba839b08475f5be0e092029bfde1ea
duckdoer <avikdeb@duckdoer.net.in>
2022-02-13 21:36:30 +0530

    # multiplication logic added

---
+++ scripts/example_01_table.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Print a multiplication table
+# Author: Avik Deb
+# Date: 13/02/2022
+# Revision: 1.0
+# Change Description: Simple shell script to print a multiplication table
+
+echo "Enter any number"
+read number
+counter=1
+while [ $counter -le 10 ]
+do
+    multiply=$(($number*$counter))
+    echo "$number X $counter = $multiply"
+    counter=$(($counter+1))
+done
