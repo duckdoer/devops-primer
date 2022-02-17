ec41fc5068abafda95b46768e20367e697e4f7fc
duckdoer <avikdeb@duckdoer.net.in>
2022-02-17 19:23:24 +0530

    # Added log level info and create logic

---
+++ scripts/clean_student_home_v1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# create_student_home.sh
+# Script to delete student directories named by roll no.
+#
+# Author: Avik Deb
+# Date: 13/02/2022
+
+echo "[INFO] Running program: $0 "
+rollnums=$*
+echo "[INIT] Deleting $# directories >> $rollnums"
+rmdir $rollnums
+echo "[SUCCESS] Directories ($#) deleted are: $rollnums."
